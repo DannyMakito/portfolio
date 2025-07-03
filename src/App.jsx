258a72531c035193237c661d3346dde94a4d78ff
dannymakito <dmakito662@gmail.com>
2025-07-03 03:00:30 +0200

    contain sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navbar from './sections/Navbar'
 import Hero from './sections/Hero'
+import About from './sections/About'
+import Projects from './sections/Projects'
 
 
 
@@ -8,13 +10,21 @@ function App() {
 
 
   return (
-    <>
+  
      <div className='container mx-auto max-w-7xl'>
           <Navbar/>
           <Hero />
+          <About />
+          <Projects />
+          <section className="min-h-screen  overflow-y-scroll no-scrollbar"/>
+          <section className="min-h-screen  overflow-x-scroll no-scrollbar"/>
+          <section className="min-h-screen  overflow-x-scroll no-scrollbar"/>
+         
+
+
      
      </div>
-    </>
+    
   )
 }
 
