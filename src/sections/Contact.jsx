ad041fd4dad907f1ce636081714750a58acb04d7
dannymakito <dmakito662@gmail.com>
2025-07-28 15:50:00 +0200

    fixed ui

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,120 +1,130 @@
-import React,{Suspense, useState} from 'react'
-import emailjs from "@emailjs/browser"
-import Alert from '../components/Alert';
-
+import { useState } from "react";
+import emailjs from "@emailjs/browser";
+import Alert from "../components/Alert";
+import { Particles } from "../components/Particles";
 const Contact = () => {
-
-const[formData, setFormData]= useState({name:" ",email:" ",message:" "});
-
-const handleChange=(e)=>{
-
-    setFormData({...formData,[e.target.name]: e.target.value})
-}
-
-const [isLoading, setIsLoading] = useState(false);
-
-const [showAlert, setShowAlert] = useState(false);
-
-
-const handleSubmit= async(e)=>{
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertType, setAlertType] = useState("success");
+  const [alertMessage, setAlertMessage] = useState("");
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const showAlertMessage = (type, message) => {
+    setAlertType(type);
+    setAlertMessage(message);
+    setShowAlert(true);
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 5000);
+  };
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    setIsLoading("true");
+    setIsLoading(true);
 
     try {
-        console.log("Form  submitted:", formData);
-        await emailjs.send("service_tz4yca4","template_9vm2ttf",{
+      console.log("From submitted:", formData);
+      await emailjs.send(
+    "service_tz4yca4","template_9vm2ttf",{
             from_name: formData.name,
             to_name:"dan",
             from_email: formData.email,
-            to_email:"makitoeyamba@gmaiil.com",
+            to_email:"makitoeyamba@gmail.com",
             message: formData.message
-        },"Rp3vOt1CHOyJRVKds");      
-        
-        setIsLoading(false);
-        setFormData({name:" ",email:" ",message:" "});
-        alert("success");
+        },"Rp3vOt1CHOyJRVKds"
+      );
+      setIsLoading(false);
+      setFormData({ name: "", email: "", message: "" });
+      showAlertMessage("success", "You message has been sent!");
     } catch (error) {
-        setIsLoading(false);
-        console.log(error);
-        alert("failed");
+      setIsLoading(false);
+      console.log(error);
+      showAlertMessage("danger", "Somthing went wrong!");
     }
-
-    
-}
-
+  };
   return (
     <section className="relative flex items-center c-space section-spacing">
-       {showAlert &&  <Alert  type={alertType}/>}
-            <div className="flex flex-col items-center justify-center max-w-md
-            border border-white/10 rounded-2xl bg-primary p-5">
-                <div className='flex flex-col items-start w-full gap-5 mb-10'>
-                    <h2 className='text-heading'>Lets talk</h2>
-                    <p className="font-normal text-neutral-400">
-                        Whether your looking to build a new website, improve your existing platform
-                        or bring a unique project to life ,Im here to help
-                    </p>
-                </div>
-
-
-                <form className='w-full' onSubmit={handleSubmit}>
-                    <div className='mb-5'>
-                        <label htmlFor='name' className='feild_label'>
-                            Full Name
-                        </label>
-                        <input 
-                        id="name"
-                        name='name'
-                        type='text'
-                        className='field-input field-input-focus'
-                        placeholder='Jonny Depp'
-                        required
-                        value={formData.name}
-                        onChange={handleChange}
-                        />
-
-                    </div>
-
-                    <div className='mb-5'>
-                        <label htmlFor='name' className='feild_label'>
-                            Email
-                        </label>
-                        <input 
-                        id="email"
-                        name='email'
-                        type='text'
-                        className='field-input field-input-focus'
-                        placeholder='jonnydepp@gmail.com'
-                        required
-                        value={formData.email}
-                        onChange={handleChange}
-                        />
-
-                    </div>
-
-                    <div className='mb-5'>
-                        <label htmlFor='name' className='feild_label'>
-                            Message
-                        </label>
-                        <textarea
-                        rows="4"
-                        id="message"
-                        name='message'
-                        type='text'
-                        className='field-input field-input-focus'
-                        placeholder='share your thoughts'
-                        required
-                        value={formData.message}
-                        onChange={handleChange}
-                        />
-
-                    </div>
-                  
-                    <button className='w-full px-1 py-3 text-lg text-center rounded-md cursor-pointer
-                    bg-radial from-lavender to-royal hover-animation' type='submit'>{!isLoading?"Send":"Sending..."}</button>
-                </form>
-            </div>
+      <Particles
+        className="absolute inset-0 -z-50"
+        quantity={100}
+        ease={80}
+        color={"#ffffff"}
+        refresh
+      />
+      {showAlert && <Alert type={alertType} text={alertMessage} />}
+      <div className="flex flex-col items-center justify-center max-w-md p-5 mx-auto border border-white/10 rounded-2xl bg-primary">
+        <div className="flex flex-col items-start w-full gap-5 mb-10">
+          <h2 className="text-heading">Let's Talk</h2>
+          <p className="font-normal text-neutral-400">
+            Whether you're loking to build a new website, improve your existing
+            platform, or bring a unique project to life, I'm here to help
+          </p>
+        </div>
+        <form className="w-full" onSubmit={handleSubmit}>
+          <div className="mb-5">
+            <label htmlFor="name" className="feild-label">
+              Full Name
+            </label>
+            <input
+              id="name"
+              name="name"
+              type="text"
+              className="field-input field-input-focus"
+              placeholder="John Doe"
+              autoComplete="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <label htmlFor="email" className="feild-label">
+              Email
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              className="field-input field-input-focus"
+              placeholder="JohnDoe@email.com"
+              autoComplete="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <label htmlFor="message" className="feild-label">
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              type="text"
+              rows="4"
+              className="field-input field-input-focus"
+              placeholder="Share your thoughts..."
+              autoComplete="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full px-1 py-3 text-lg text-center rounded-md cursor-pointer bg-radial from-lavender to-royal hover-animation"
+          >
+            {!isLoading ? "Send" : "Sending..."}
+          </button>
+        </form>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
